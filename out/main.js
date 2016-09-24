e7c707409a9ba121261a9adaf0730a03caff7d33
ServantBrea <516916849@qq.com>
2016-09-24 15:02:27 +0800

    嵌入输出金字塔的代码！

--- out/main.js
+++ out/main.js
@@ -15,8 +15,29 @@ var Greeter = (function () {
     };
     return Greeter;
 }());
+function BuildPyramid(n) {
+    var i;
+    var j;
+    var k;
+    var x = " ";
+    var y = "*";
+    for (i = 0; i < n; i++) {
+        var a = "";
+        for (j = 0; j < n - (i + 1); j++) {
+            a = a + x;
+        }
+        for (k = 0; k < i + 1; k++) {
+            a = a + y;
+            if (k < i) {
+                a = a + x;
+            }
+        }
+        console.log(a);
+    }
+}
 window.onload = function () {
     var el = document.getElementById('content');
     var greeter = new Greeter(el);
     greeter.start();
+    BuildPyramid(10);
 };
--- src/main.ts
+++ src/main.ts
@@ -21,8 +21,31 @@ class Greeter {
 
 }
 
+function BuildPyramid(n:number):void {
+    var i:number;
+    var j:number;
+    var k:number;
+    var x=" ";
+    var y="*";
+    
+    for(i=0;i<n;i++) {
+        var a="";
+        for(j=0;j<n-(i+1);j++) {
+            a=a+x;
+        }
+        for(k=0;k<i+1;k++) {
+            a=a+y;
+            if(k<i){
+                a=a+x;
+            }
+        }
+        console.log(a);
+    }
+}
+
 window.onload = () => {
     var el = document.getElementById('content');
     var greeter = new Greeter(el);
     greeter.start();
+    BuildPyramid(10);
 };
